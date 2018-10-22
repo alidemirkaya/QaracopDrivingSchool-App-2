08ccef2270c47d22f177bc593ec034d0b4c678eb
Ali Demirkaya <alidmrky@gmail.com>
2018-10-22 21:06:31 +0300

    Sonartik

--- QaracopDrivingSchool App/Class1.cs
+++ QaracopDrivingSchool App/Class1.cs
@@ -18,5 +18,10 @@ namespace QaracopDrivingSchool_App
             Form1 form = new Form1();
             form.Show();
         }
+        public void Gonder3()
+        {
+            Form1 form = new Form1();
+            form.Show();
+        }
     }
 }
